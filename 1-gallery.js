57740edfc4b8e2eafdba917aaf85d99379c52665
DP1106081 <dpkds161@gmail.com>
2026-09-07 14:31:46 +0000

    Deploying to gh-pages from @ DP1106081/goit-js-hw-09@871267ac872d029b244f25e2e8af59ca036f16a1 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+/* empty css                      */import"./assets/modulepreload-polyfill-P2Xu9kJm.js";console.log(`gsfhdhfgd`);
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+/* empty css                      */import"./assets/modulepreload-polyfill-P2Xu9kJm.js";console.log(`dcvdvdvdv`);
+//# sourceMappingURL=2-form.js.map
