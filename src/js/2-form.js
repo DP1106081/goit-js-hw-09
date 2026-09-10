ecb922b30a313f64a110c215382224a8c0158fae
DP <dpkds161@gmail.com>
2026-09-10 12:05:43 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,40 @@
-console.log('dcvdvdvdv');
-console.log('aaaaaaaa');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = { email: '', message: '' };
+const form = document.querySelector('.feedback-form');
+
+function populateForm() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+populateForm();
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Submitted formData:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
